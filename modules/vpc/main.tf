f8e17e43d56f6540afa61839e1eeaf88d71fb4be
tom-klimovski-kappa <tom.klimovski@kappadata.io>
2019-12-28 19:50:45 -0500

    cleanup

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,17 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 module "vpc" {
   source  = "terraform-google-modules/network/google"
@@ -32,17 +18,3 @@ module "vpc" {
     "${var.env}-subnet-01" = []
   }
 }
-
-for i in 0 1 2; do
-  gcloud compute instances create controller-${i} \
-    --async \
-    --boot-disk-size 200GB \
-    --can-ip-forward \
-    --image-family ubuntu-1804-lts \
-    --image-project ubuntu-os-cloud \
-    --machine-type n1-standard-1 \
-    --private-network-ip 10.240.0.1${i} \
-    --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
-    --subnet dev-subnet-01 \
-    --tags kubernetes-the-hard-way,controller
-done
